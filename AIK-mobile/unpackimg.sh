b64463601fc79d50d98223e772415c070c6bbf3e
Павел Шляпин <blackeangel@mail.ru>
2026-09-08 16:56:04 +0300

    Обновить unpackimg.sh

--- AIK-mobile/unpackimg.sh
+++ AIK-mobile/unpackimg.sh
@@ -3,27 +3,30 @@
 # osm0sis @ xda-developers
 
 case $1 in
-  --help) echo "usage: unpackimg.sh <file>"; return 1;
+  --help) echo "...usage: unpackimg.sh <file>"; return 1;
 esac;
 
 case $0 in
   *.sh) aik="$0";;
-     *) aik="$(lsof -p $$ 2>/dev/null | grep -o '/.*unpackimg.sh$')";;
+     *) aik="$(lsof -p $$ 2>/dev/null | $bb grep -o '/.*unpackimg.sh$')";;
 esac;
 aik="$(dirname "$(readlink -f "$aik")")";
 bin="$aik/bin";
 cur="$(readlink -f "$PWD")";
 
 cleanup() { $aik/cleanup.sh --quiet; }
-abort() { cd $aik; echo "Error!"; . ker_ver; }
+abort() { cd $aik; echo "...Error!";} # . ker_ver; }
 
 cd $aik;
 bb=$bin/busybox;
 chmod -R 755 $bin *.sh;
-chmod 644 $bin/magic $bin/androidbootimg.magic $bin/boot_signer-dexed.jar $bin/module.prop $bin/ramdisk.img $bin/avb/* $bin/chromeos/*;
+#chmod 644 $bin/magic $bin/androidbootimg.magic $bin/boot_signer-dexed.jar $bin/module.prop $bin/ramdisk.img $bin/avb/* $bin/chromeos/*;
+
+chmod 644 $bin/magic $bin/androidbootimg.magic $bin/boot_signer-dexed.jar $bin/module.prop $bin/avb/* $bin/chromeos/*;
 
 [ ! -f $bb ] && bb=busybox;
 
+unset img
 img="$1";
 [ -f "$cur/$1" ] && img="$cur/$1";
 if [ ! "$img" ]; then
@@ -37,7 +40,7 @@ if [ ! "$img" ]; then
 fi;
 img="$(readlink -f "$img")";
 if [ ! -f "$img" ]; then
-  echo "No image file supplied.";
+  echo "...No image file supplied.";
   abort;
   return 1;
 fi;
@@ -47,22 +50,22 @@ echo "\nAndroid Image Kitchen - UnpackImg Script";
 echo "by osm0sis @ xda-developers\n";
 
 file=$($bb basename "$img");
-echo "Supplied image: $file\n";
+echo "...Supplied image: $file\n";
 
 if [ -d split_img -o -d ramdisk ]; then
-  echo "Removing old work folders and files...\n";
+  echo "...Removing old work folders and files...\n";
   cleanup;
 fi;
 
-echo "Setting up work folders...\n";
+echo "...Setting up work folders...\n";
 mkdir split_img ramdisk;
 chmod 755 split_img ramdisk;
-echo "run remount.sh to remount the current image's unpacked ramdisk" > ramdisk/README;
-chmod 666 ramdisk/README;
-cp -fp $bin/remount.sh ramdisk/remount.sh;
-cp -f $bin/ramdisk.img split_img/.aik-ramdisk.img;
+#echo "run remount.sh to remount the current image's unpacked ramdisk" > ramdisk/README;
+#chmod 666 ramdisk/README;
+#$bb cp -fp $bin/remount.sh ramdisk/remount.sh;
+#$bb cp -f $bin/ramdisk.img split_img/.aik-ramdisk.img;
 
-$bin/remount.sh --mount-only || return 1;
+#$bin/remount.sh --mount-only || return 1;
 
 cd split_img;
 filesize=$($bb wc -c < "$img");
@@ -70,8 +73,8 @@ echo "$filesize" > "$file-origsize";
 imgtest="$($bin/file -m $bin/androidbootimg.magic "$img" 2>/dev/null | $bb cut -d: -f2-)";
 if [ "$(echo $imgtest | $bb awk '{ print $2 }' | $bb cut -d, -f1)" == "signing" ]; then
   echo $imgtest | $bb awk '{ print $1 }' > "$file-sigtype";
-  sigtype=$(cat "$file-sigtype");
-  echo "Signature with \"$sigtype\" type detected, removing...\n";
+  sigtype=$($bb cat "$file-sigtype");
+  echo "...Signature with \"$sigtype\" type detected, removing...\n";
   case $sigtype in
     BLOB)
       $bb cp -f "$img" "$file";
@@ -101,22 +104,22 @@ imgtest="$($bin/file -m $bin/androidbootimg.magic "$img" 2>/dev/null | $bb cut -
 if [ "$(echo $imgtest | $bb awk '{ print $2 }' | $bb cut -d, -f1)" == "bootimg" ]; then
   [ "$(echo $imgtest | $bb awk '{ print $3 }')" == "PXA" ] && typesuffix=-PXA;
   echo "$(echo $imgtest | $bb awk '{ print $1 }')$typesuffix" > "$file-imgtype";
-  imgtype=$(cat "$file-imgtype");
+  imgtype=$($bb cat "$file-imgtype");
 else
   cd ..;
   cleanup;
-  echo "Unrecognized format.";
+  echo "...Unrecognized format.";
   abort;
   return 1;
 fi;
-echo "Image type: $imgtype\n";
+echo "...Image type: $imgtype\n";
 
 case $imgtype in
   AOSP*|ELF|KRNL|OSIP|U-Boot) ;;
   *)
     cd ..;
     cleanup;
-    echo "Unsupported format.";
+    echo "...Unsupported format.";
     abort;
     return 1;
   ;;
@@ -125,20 +128,20 @@ esac;
 case $(echo $imgtest | $bb awk '{ print $3 }') in
   LOKI)
     echo $imgtest | $bb awk '{ print $5 }' | $bb cut -d\( -f2 | $bb cut -d\) -f1 > "$file-lokitype";
-    lokitype=$(cat "$file-lokitype");
-    echo "Loki patch with \"$lokitype\" type detected, reverting...\n";
-    echo "Warning: A dump of your device's aboot.img is required to re-Loki!\n";
+    lokitype=$($bb cat "$file-lokitype");
+    echo "...Loki patch with \"$lokitype\" type detected, reverting...\n";
+    echo "...Warning: A dump of your device's aboot.img is required to re-Loki!\n";
     $bin/loki_tool unlok "$img" "$file" >/dev/null;
     img="$file";
   ;;
   AMONET)
-    echo "Amonet patch detected, reverting...\n";
+    echo "...Amonet patch detected, reverting...\n";
     $bb dd bs=2048 count=1 conv=notrunc if="$img" of="$file-microloader.bin" 2>/dev/null;
     $bb dd bs=1024 skip=1 conv=notrunc if="$file-microloader.bin" of="$file-head" 2>/dev/null;
     $bb truncate -s 1024 "$file-microloader.bin";
     $bb truncate -s 2048 "$file-head";
     $bb dd bs=2048 skip=1 conv=notrunc if="$img" of="$file-tail" 2>/dev/null;
-    cat "$file-head" "$file-tail" > "$file";
+   $bb cat "$file-head" "$file-tail" > "$file";
     rm -f "$file-head" "$file-tail";
     img="$file";
   ;;
@@ -159,7 +162,7 @@ esac;
 tailtype="$(echo $tailtest | $bb awk '{ print $1 }')";
 case $tailtype in
   AVB*)
-    echo "Signature with \"$tailtype\" type detected.\n";
+    echo "...Signature with \"$tailtype\" type detected.\n";
     case $tailtype in
       *v1)
         echo $tailtype > "$file-sigtype";
@@ -168,7 +171,7 @@ case $tailtype in
     esac;
   ;;
   Bump|SEAndroid)
-    echo "Footer with \"$tailtype\" type detected.\n";
+    echo "...Footer with \"$tailtype\" type detected.\n";
     echo $tailtype > "$file-tailtype";
   ;;
 esac;
@@ -176,18 +179,133 @@ esac;
 if [ "$imgtype" == "U-Boot" ]; then
   imgsize=$(($($bb printf '%d\n' 0x$($bb hexdump -n 4 -s 12 -e '16/1 "%02x""\n"' "$img")) + 64));
   if [ "$filesize" != "$imgsize" ]; then
-    echo "Trimming...\n";
+    echo "...Trimming...\n";
     $bb dd bs=$imgsize count=1 conv=notrunc if="$img" of="$file" 2>/dev/null;
     img="$file";
   fi;
 fi;
 
-echo 'Splitting image to "split_img/"...';
+echo '...Splitting image to "split_img/"...\n';
 case $imgtype in
   AOSP_VNDR) vendor=vendor_;;
 esac;
 case $imgtype in
-  AOSP|AOSP_VNDR) $bin/unpackbootimg -i "$img";;
+  AOSP|AOSP_VNDR) #$bin/unpackbootimg -i "$img" &> /dev/null
+  
+  unboot --boot_img "$img" --out config --format mkbootimg > conf.txt
+  #unboot --boot_img "$img" --out config --format mkbootimg > conf1.txt
+ 
+$bb cp -f conf.txt config/conf.txt
+#$bb cp -f conf1.txt config/conf1.txt
+if [ -f config/bootconfig ]; then
+$bb cp -f config/bootconfig ./
+fi
+
+  #if [ ! -z "$($bb awk '/vendor boot image header version:/ { print $6 }' /data/local/AIK-mobile/split_img/config/conf.txt)" == "4" ]; then
+  
+ aik_new_dir=/data/local/AIK-mobile/split_img
+ r_dir=/data/local/AIK-mobile
+ ram_dir=/data/local/AIK-mobile/split_img/config
+ #echo "1" > "$ram_dir"/SETPERM.txt
+ #/data/local/python31/usr/bin/extract-dtb "$ram_dir"/dtb -o "$ram_dir" &> /dev/null
+ if [ ! -z "$($bb cat "$ram_dir"/conf.txt | $bb grep "boot magic: VNDRBOOT")" ]; then
+  v_b="1"
+  fi
+ 
+ header_version="$($bb awk '/vendor boot image header version:/ { print $6 }' "$ram_dir"/conf.txt)"
+ 
+ if [ "$v_b" == "1" ]; then
+ $bin/unpackbootimg -i "$img" &> /dev/null
+ 
+  [ "$header_version" == "4" ] && echo "0" > config/MAG.txt;
+
+ #echo
+ print_cmdline=$($bb cat config/conf.txt | $bb grep "^\--vendor_cmdline" | $bb sed 's!^--vendor_cmdline !!')
+ echo "cmdline = $print_cmdline"
+ 
+ print_board=$($bb cat config/conf.txt | $bb awk '/^\--board/ { print $2 }')
+ echo  "board = $print_board"
+ 
+ print_base=$($bb cat config/conf.txt | $bb grep "^\--base")
+ echo "$print_base" | $bb sed 's!^--!!' | $bb awk '{ print $1" ""="" "$2}'
+ 
+ print_pagesize=$($bb cat config/conf.txt | $bb grep "^\--pagesize")
+ echo "$print_pagesize" | $bb sed 's!^--!!' | $bb awk '{ print $1" ""="" "$2}'
+ 
+ print_kerneloff=$($bb cat config/conf.txt | $bb grep "^\--kernel_offset")
+ echo "$print_kerneloff" | $bb sed 's!^--!!' | $bb awk '{ print $1" ""="" "$2}'
+ 
+ print_ramdiskoff=$($bb cat config/conf.txt | $bb grep "^\--ramdisk_offset")
+ echo "$print_ramdiskoff" | $bb sed 's!^--!!' | $bb awk '{ print $1" ""="" "$2}'
+ 
+ print_tagsoff=$($bb cat config/conf.txt | $bb grep "^\--tags_offset")
+ echo "$print_tagsoff" | $bb sed 's!^--!!' | $bb awk '{ print $1" ""="" "$2}'
+ 
+ print_dtboff=$($bb cat config/conf.txt | $bb grep "^\--dtb_offset")
+ echo "$print_dtboff" | $bb sed 's!^--!!' | $bb awk '{ print $1" ""="" "$2}'
+ 
+ print_hdrver="--header_version $header_version"
+ echo "$print_hdrver" | $bb sed 's!^--!!' | $bb awk '{ print $1" ""="" "$2}'
+ 
+ else
+ $bin/unpackbootimg -i "$img"
+ fi
+ 
+ cd "$ram_dir"
+ 
+ #$bb ls *_dtb* | while read a; do
+ #rem_name="$(echo "$a" | $bb awk -F"_" '{ print "dtb_"$1 }')"
+#$bb mv -f "$a" "$rem_name"
+#done
+#$bb rm -f 00_kernel
+ 
+ if [ ! -z "$($bb find -type f | $bb grep "ramdisk01")" ]; then
+ #echo "true" > fragment.txt
+ frag_real="true"
+ > perm00.txt
+ > perm01.txt
+$bb find -name "*ramdisk[0-9][0-9]" -type f | while read rd; do
+ name_rd="$(echo "$rd" | $bb grep -o "ramdisk[0-9]*")"
+ r_num="$(echo "$name_rd" | $bb sed 's!ramdisk!!')"
+ "$bin"/file -m "$bin"/magic "$rd" 2>/dev/null | $bb cut -d: -f2 | $bb awk '{ print $1 }' > "$name_rd"_dec.log
+ 
+ compout="$($bb cat "$name_rd"_dec.log)"
+
+case "$compout" in
+    zstd) compout=zstd;;
+    gzip) compout=gz;;
+    lzop) compout=lzo;;
+    xz|lz4|lzma) compout="$compout";;
+    bzip2) compout=bz2;;
+    lz4-l) compout=lz4;;
+    *) abort; exit 1;;
+  esac;
+  compout=".$compout"
+echo "$name_rd"-new.cpio"$compout" > REPLACE_"$name_rd".txt
+
+ rm -rf "$r_dir"/"$name_rd"
+ mkdir "$r_dir"/"$name_rd"
+ cd "$r_dir"/"$name_rd"
+
+ bootpatch decompress "$ram_dir"/"$rd" "$ram_dir"/"$rd".cpio &>/dev/null && bootpatch cpio "$ram_dir"/"$rd".cpio extract &>/dev/null || bootpatch cpio "$ram_dir"/"$rd" extract &>/dev/null
+ 
+ #$bb find | $bb xargs $bb stat -c '%n %u %g %a' | $bb sed 's!^./!!' >> "$ram_dir"/perm"$r_num".txt
+ 
+ cd "$r_dir"
+ $bb find "$name_rd" -type d -o -type f | $bb xargs $bb stat -c '%n %u %g %a' | $bb sed 's!^./!!' >> "$ram_dir"/perm"$r_num".txt
+ cd "$ram_dir"
+done
+ cd "$aik_new_dir"
+ echo " Удалить *ramdisk01 или ramdisk01_dec.log" > config/DELETE_ramdisk01.txt
+ echo "0" > config/UNITE_ramdisk.txt
+ 
+ else
+ #echo "false" > fragment.txt
+ frag_real="false"
+ cd "$aik_new_dir"
+ fi;;
+ #fi
+
   AOSP-PXA) $bin/pxa-unpackbootimg -i "$img";;
   ELF)
     mkdir elftool_out;
@@ -217,7 +335,7 @@ case $imgtype in
     rm -f "$file-header";
     $bin/dumpimage -p 0 -o "$file-kernel" "$img";
     [ $? != 0 ] && error=1;
-    case $(cat "$file-type") in
+    case $($bb cat "$file-type") in
       Multi) $bin/dumpimage -p 1 -o "$file-ramdisk" "$img";;
       RAMDisk) $bb mv -f "$file-kernel" "$file-ramdisk";;
       *) touch "$file-ramdisk";;
@@ -233,7 +351,7 @@ fi;
 
 if [ -f *-kernel ] && [ "$($bin/file -m $bin/androidbootimg.magic *-kernel 2>/dev/null | $bb cut -d: -f2 | $bb awk '{ print $1 }')" == "MTK" ]; then
   mtk=1;
-  echo "\nMTK header found in kernel, removing...";
+  echo "\n...MTK header found in kernel, removing...";
   $bb dd bs=512 skip=1 conv=notrunc if="$file-kernel" of=tempkern 2>/dev/null;
   $bb mv -f tempkern "$file-kernel";
 fi;
@@ -241,16 +359,16 @@ mtktest="$($bin/file -m $bin/androidbootimg.magic *-*ramdisk 2>/dev/null | $bb c
 mtktype=$(echo $mtktest | $bb awk '{ print $3 }');
 if [ "$(echo $mtktest | $bb awk '{ print $1 }')" == "MTK" ]; then
   if [ ! "$mtk" ]; then
-    echo "\nWarning: No MTK header found in kernel!";
+    echo "\n...Warning: No MTK header found in kernel!";
     mtk=1;
   fi;
-  echo "MTK header found in \"$mtktype\" type ramdisk, removing...";
+  echo "...MTK header found in \"$mtktype\" type ramdisk, removing...";
   $bb dd bs=512 skip=1 conv=notrunc if="$(ls *-*ramdisk)" of=temprd 2>/dev/null;
   $bb mv -f temprd "$(ls *-*ramdisk)";
 else
   if [ "$mtk" ]; then
     if [ ! "$mtktype" ]; then
-      echo 'Warning: No MTK header found in ramdisk, assuming "rootfs" type!';
+      echo '...Warning: No MTK header found in ramdisk, assuming "rootfs" type!';
       mtktype="rootfs";
     fi;
   fi;
@@ -263,17 +381,17 @@ if [ -f *-dt ]; then
   if [ "$imgtype" == "ELF" ]; then
     case $dttest in
       QCDT|ELF) ;;
-      *) echo "\nNon-QC DTB found, packing kernel and appending...";
+      *) echo "\n...Non-QC DTB found, packing kernel and appending...";
          $bb gzip "$file-kernel";
          $bb mv -f "$file-kernel.gz" "$file-kernel";
-         cat "$file-dt" >> "$file-kernel";
+        $bb cat "$file-dt" >> "$file-kernel";
          rm -f "$file-dt"*;;
     esac;
   fi;
 fi;
 
 $bin/file -m $bin/magic *-*ramdisk 2>/dev/null | $bb cut -d: -f2 | $bb awk '{ print $1 }' > "$file-${vendor}ramdiskcomp";
-ramdiskcomp=`cat *-*ramdiskcomp`;
+ramdiskcomp=`$bb cat *-*ramdiskcomp`;
 unpackcmd="$bb $ramdiskcomp -dc";
 compext=$ramdiskcomp;
 case $ramdiskcomp in
@@ -284,7 +402,8 @@ case $ramdiskcomp in
   bzip2) compext=bz2;;
   lz4) unpackcmd="$bin/lz4 -dcq";;
   lz4-l) unpackcmd="$bin/lz4 -dcq"; compext=lz4;;
-  cpio) unpackcmd="cat"; compext="";;
+  zstd) unpackcmd="$bin/zstd -dc"; compext=zst;;
+  cpio) unpackcmd="$bb cat"; compext="";;
   empty) compext=empty;;
   *) compext="";;
 esac;
@@ -294,32 +413,49 @@ fi;
 $bb mv -f "$(ls *-*ramdisk)" "$file-${vendor}ramdisk.cpio$compext" 2>/dev/null;
 cd ..;
 if [ "$ramdiskcomp" == "data" ]; then
-  echo "Unrecognized format.";
+  echo "...Unrecognized format.";
   abort;
   return 1;
 fi;
 
 if [ "$ramdiskcomp" == "empty" ]; then
-  echo "\nWarning: No ramdisk found to be unpacked!";
+  echo "\n...Warning: No ramdisk found to be unpacked!";
 else
-  echo '\nUnpacking ramdisk to "ramdisk/"...\n';
-  echo "Compression used: $ramdiskcomp";
+  echo '\n...Unpacking ramdisk to "ramdisk/"...\n';
+  echo "...Compression used: $ramdiskcomp";
+  #echo "Unpacking ramdisk to ramdisk/...";
   if [ ! "$compext" -a ! "$ramdiskcomp" == "cpio" ]; then
-    echo "Unsupported format.";
+    echo "...Unsupported format.";
     abort;
     return 1;
   fi;
+  #cd ramdisk;
+  #$bb rm -rf lost+found
+  #$unpackcmd "../split_img/$file-${vendor}ramdisk.cpio$compext" | EXTRACT_UNSAFE_SYMLINKS=1 cpio -i -d 2>&1;
+  
   cd ramdisk;
-  $unpackcmd "../split_img/$file-${vendor}ramdisk.cpio$compext" | EXTRACT_UNSAFE_SYMLINKS=1 $bb cpio -i -d 2>&1;
+  $bb rm -rf lost+found
+  bootpatch decompress ../split_img/$file-${vendor}ramdisk.cpio$compext ../split_img/$file-${vendor}ramdisk_m.cpio &>/dev/null && bootpatch cpio ../split_img/$file-${vendor}ramdisk_m.cpio extract &>/dev/null || bootpatch cpio ../split_img/$file-${vendor}ramdisk.cpio$compext extract &>/dev/null
+  
+  
   if [ $? != 0 ]; then
     cd ..;
     abort;
     return 1;
   fi;
   cd ..;
-fi;
-. ker_ver;
+  $bb find ramdisk -type d -o -type f | $bb xargs $bb stat -c '%n %u %g %a' | $bb sed 's!^./!!' >> "$aik"/split_img/config/perm.txt
+  #echo "ramdisk-new.cpio$compext" > split_img/config/OUTNEW_ramdisk.txt
+  if [ "$header_version" == "4" -a "$frag_real" == "false" -a -s "$ram_dir"/*ramdisk00  ]; then
+  echo " ramdisk01-new.cpio$compext" > split_img/config/ADDNEW_ramdisk01.txt
+  echo " ramdisk-new.cpio$compext" > split_img/config/REPLACE_ramdisk.txt
+  elif [ "$header_version" == "4" -a "$frag_real" == "false" -a ! -s "$ram_dir"/*ramdisk00  ]; then
+  echo " ramdisk-new.cpio$compext" > split_img/config/REPLACE_ramdisk.txt
+    elif [ "$header_version" != "4" -a "$frag_real" == "false" ]; then
+  echo " ramdisk-new.cpio$compext" > split_img/config/REPLACE_ramdisk.txt
+  fi
+ fi;
+#. ker_ver;
 
-echo "\nDone!";
+echo "\n...Done!";
 return 0;
-
